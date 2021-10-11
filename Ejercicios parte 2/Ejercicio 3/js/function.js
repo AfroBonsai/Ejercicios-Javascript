b1b3e1e49f8a8498abb7973ed058f86d94518c55
AfroBonsai <geozam89@gmail.com>
2021-10-11 10:24:11 +0200

    Ejercicio 3 corregido

--- Ejercicios parte 2/Ejercicio 3/js/function.js
+++ Ejercicios parte 2/Ejercicio 3/js/function.js
@@ -12,10 +12,33 @@ Version: 1.0
 
 // ---------- Input ----------
 
+const num = parseInt (prompt("Introduce un número para saber si es primo:"));
 
+let esPrimo = true;
 
 // ---------- Process ----------
 
+if (num === 1) {
+    console.log("El número 1 no es ni primo ni compuesto.");
+}
 
+else if (num > 1) {
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            esPrimo = false;
+            break;
+        }
+    }
 
 // ---------- Output ----------
+
+    if (esPrimo) {
+        console.log("El número " + num + " SÍ es un número primo.");
+        alert("El número " + num + " SÍ es un número primo.");
+    } else {
+        console.log("El número " + num + " NO es un número primo.");
+        alert("El número " + num + " NO es un número primo.");
+    }
+}
+
